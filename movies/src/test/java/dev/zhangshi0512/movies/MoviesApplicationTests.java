6bdf380832c02684097892eb363c2c0f3b025a4c
Simon Zhang <100053535+zhangshi0512@users.noreply.github.com>
2023-02-24 19:46:57 -0800

    Add files via upload

---
+++ movies/src/test/java/dev/zhangshi0512/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zhangshi0512.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
